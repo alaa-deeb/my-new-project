1ac4ed03a1749f09b8e7bee2260a7c9cb144331e
alaa deeb <alaaahmaddeeb60@gmail.com>
2025-09-06 11:31:58 +0300

    edit the app.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import logo from './alaa.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="profile-container">
+          <div className="circle">
+            <img src={logo} className="App-logo" alt="alaa" />
+          </div>
+          <h1>ALaa Ahmad Deeb</h1>
+          <h2>Engineer at a remittance company</h2>
+          <p>alaaahmaddeeb60@gmail.com</p>
+          <p>+963952752398</p>
+        </div>
       </header>
+      <footer className="App-footer">
+        by alaa
+      </footer>
     </div>
   );
 }
